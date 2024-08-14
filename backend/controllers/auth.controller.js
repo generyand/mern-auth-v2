b40f508f74ac3bf9c8bf3873052b1a5835d6f75b
generyand <generyan.dep@gmail.com>
2024-08-14 17:06:35 +0800

    1:16:14

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificationEmail } from "../mailtrap/emails.js";
+import { sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/emails.js";
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -47,6 +47,34 @@ export const signup = async (req, res) => {
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
+};
+
+export const verifyEmail = async (req, res) => {
+  const { code } = req.body;
+
+  try {
+    const user = await User.findOne({
+      verificationToken: code,
+      verificationTokenExpiresAt: {
+        $gt: Date.now(),
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid or expired verification code",
+      });
+    }
+
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    user.verificationTokenExpiresAt = undefined;
+
+    await user.save();
+
+    await sendWelcomeEmail(user.email, user.name);
+  } catch (error) {}
 };
 
 export const login = async (req, res) => {
--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -14,7 +14,6 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         verificationToken
       ),
       category: "Email Verification",
-      
     });
     //   .then(console.log)
     //   .catch(console.error);
@@ -23,5 +22,28 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     console.log(`Error: ${error}`);
 
     throw new Error(error);
+  }
+};
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: `Welcome ${name}`,
+      template_uuid: "2214bd1a-f648-4073-8b3a-8c4da95ed022",
+      template_variables: {
+        company_info_name: "PGITS",
+        name: name,
+      },
+    });
+
+    console.log("Welcome Email sent successfuly", response);
+  } catch (error) {
+    console.error("Error sending welcome email", error);
+
+    throw new Error("Error sending welcome email", error);
   }
 };
